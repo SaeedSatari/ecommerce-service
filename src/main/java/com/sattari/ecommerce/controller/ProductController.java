ea87d963cee2361d7f5875968f041625b857e8bd
SaeedSatari <saeedsatari93@gmail.com>
2020-06-14 14:15:12 +0200

    -added crud apis

--- src/main/java/com/sattari/ecommerce/controller/ProductController.java
+++ src/main/java/com/sattari/ecommerce/controller/ProductController.java
@@ -6,14 +6,15 @@ import com.sattari.ecommerce.controller.response.ProductResponse;
 import com.sattari.ecommerce.dal.entity.Product;
 import com.sattari.ecommerce.service.ProductService;
 import com.sattari.ecommerce.service.mapper.ProductMapper;
-import java.util.List;
-import java.util.stream.Collectors;
-import javassist.NotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Saeed Sattari
@@ -22,61 +23,69 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/products")
 @CrossOrigin
 public class ProductController {
-  private final ProductService productService;
+    private final ProductService productService;
 
-  public ProductController(ProductService productService) {
-    this.productService = productService;
-  }
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
-  @GetMapping
-  public PagedProductsResponse getPagedProducts(
-    @RequestParam int page,
-    @RequestParam int size
-  ) {
-    PagedProductsResponse response = new PagedProductsResponse();
-    Pageable paging = PageRequest.of(page, size);
-    List<Product> products = productService.fetchProducts();
-    Page<Product> pagedProducts = new PageImpl<>(products, paging, products.size());
-    response.setPage(initiatePageResponse(pagedProducts));
-    response.setProducts(
-      pagedProducts
-        .stream()
-        .map(ProductMapper.MAPPER::productToProductResponse)
-        .collect(Collectors.toList())
-    );
-    return response;
-  }
+    @GetMapping
+    public PagedProductsResponse getPagedProducts(@RequestParam int page, @RequestParam int size) {
+        PagedProductsResponse response = new PagedProductsResponse();
+        Pageable paging = PageRequest.of(page, size);
+        List<Product> products = productService.fetchProducts();
+        Page<Product> pagedProducts = new PageImpl<>(products, paging, products.size());
+        response.setPage(initiatePageResponse(pagedProducts));
+        response.setProducts(pagedProducts.stream().map(ProductMapper.MAPPER::productToProductResponse).collect(Collectors.toList()));
+        return response;
+    }
 
-  private PageResponse initiatePageResponse(Page<Product> pagedProducts) {
-    PageResponse pageResponse = new PageResponse();
-    pageResponse.setTotalPages(pagedProducts.getTotalPages());
-    pageResponse.setTotalElements(pagedProducts.getTotalElements());
-    pageResponse.setSize(pagedProducts.getSize());
-    pageResponse.setNumber(pagedProducts.getNumber());
-    return pageResponse;
-  }
+    private PageResponse initiatePageResponse(Page<Product> pagedProducts) {
+        PageResponse pageResponse = new PageResponse();
+        pageResponse.setTotalPages(pagedProducts.getTotalPages());
+        pageResponse.setTotalElements(pagedProducts.getTotalElements());
+        pageResponse.setSize(pagedProducts.getSize());
+        pageResponse.setNumber(pagedProducts.getNumber());
+        return pageResponse;
+    }
 
-  @GetMapping("/{productId}")
-  public ProductResponse getProductDetails(@PathVariable String productId) {
-    Product product = productService.fetchProduct(productId);
-    return ProductMapper.MAPPER.productToProductResponse(product);
-  }
+    @GetMapping("/{productId}")
+    public ProductResponse getProductDetails(@PathVariable String productId) {
+        Product product = productService.fetchProduct(productId);
+        return ProductMapper.MAPPER.productToProductResponse(product);
+    }
 
-  @GetMapping("/search/categoryId")
-  public List<ProductResponse> searchByCategoryId(@RequestParam String id) {
-    List<Product> products = productService.findByCategoryId(id);
-    return products
-      .stream()
-      .map(ProductMapper.MAPPER::productToProductResponse)
-      .collect(Collectors.toList());
-  }
+    @GetMapping("/search/categoryId")
+    public List<ProductResponse> searchByCategoryId(@RequestParam String id) {
+        List<Product> products = productService.findByCategoryId(id);
+        return products.stream().map(ProductMapper.MAPPER::productToProductResponse).collect(Collectors.toList());
+    }
 
-  @GetMapping("/search/keyword")
-  public List<ProductResponse> searchByKeyword(@RequestParam("name") String name) {
-    List<Product> products = productService.findByKeyword(name);
-    return products
-      .stream()
-      .map(ProductMapper.MAPPER::productToProductResponse)
-      .collect(Collectors.toList());
-  }
+    @GetMapping("/search/keyword")
+    public List<ProductResponse> searchByKeyword(@RequestParam("name") String name) {
+        List<Product> products = productService.findByKeyword(name);
+        return products.stream().map(ProductMapper.MAPPER::productToProductResponse).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+        Product createdProduct = productService.createProduct(productRequest);
+        return ProductMapper.MAPPER.productToProductResponse(createdProduct);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UpdatedProductResponse editProduct(@RequestBody ProductRequest productRequest, @PathVariable Long id) {
+        UpdatedProductResponse response = new UpdatedProductResponse();
+        Product product = productService.editProduct(productRequest, id);
+        response.setSuccessful("Product Updated Successfully");
+        response.setProductResponse(ProductMapper.MAPPER.productToProductResponse(product));
+        return response;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable("id") Long id) {
+        return productService.deleteProduct(id);
+    }
 }
--- src/main/java/com/sattari/ecommerce/service/ProductService.java
+++ src/main/java/com/sattari/ecommerce/service/ProductService.java
@@ -3,10 +3,11 @@ package com.sattari.ecommerce.service;
 import com.sattari.ecommerce.commons.exceptions.EntityNotFoundException;
 import com.sattari.ecommerce.dal.entity.Product;
 import com.sattari.ecommerce.dal.repository.ProductRepository;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Saeed Sattari
@@ -14,49 +15,45 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(readOnly = true)
 public class ProductService {
-  private final ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
-  public ProductService(ProductRepository productRepository) {
-    this.productRepository = productRepository;
-  }
-
-  public List<Product> fetchProducts() {
-    List<Product> fetchedProducts = productRepository.findAll();
-    if (fetchedProducts.isEmpty()) {
-      throw new EntityNotFoundException(Product.class);
-    } else {
-      return fetchedProducts;
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
-  }
 
-  public Product fetchProduct(String productId) {
-    Optional<Product> optionalProduct = productRepository.findById(
-      Long.valueOf(productId)
-    );
-    if (optionalProduct.isPresent()) {
-      return optionalProduct.get();
-    } else {
-      throw new EntityNotFoundException(Product.class, "productId", productId);
+    public List<Product> fetchProducts() {
+        List<Product> fetchedProducts = productRepository.findAll();
+        if (fetchedProducts.isEmpty()) {
+            throw new EntityNotFoundException(Product.class);
+        } else {
+            return fetchedProducts;
+        }
     }
-  }
 
-  public List<Product> findByCategoryId(String categoryId) {
-    List<Product> fetchedProducts = productRepository.findByCategoryId(
-      Long.valueOf(categoryId)
-    );
-    if (fetchedProducts.isEmpty()) {
-      throw new EntityNotFoundException(Product.class, "categoryId", categoryId);
-    } else {
-      return fetchedProducts;
+    public Product fetchProduct(String productId) {
+        Optional<Product> optionalProduct = productRepository.findById(Long.valueOf(productId));
+        if (optionalProduct.isPresent()) {
+            return optionalProduct.get();
+        } else {
+            throw new EntityNotFoundException(Product.class, "productId", productId);
+        }
     }
-  }
 
-  public List<Product> findByKeyword(String name) {
-    List<Product> fetchedProducts = productRepository.findByNameContaining(name);
-    if (fetchedProducts.isEmpty()) {
-      throw new EntityNotFoundException(Product.class, "name", name);
-    } else {
-      return fetchedProducts;
+    public List<Product> findByCategoryId(String categoryId) {
+        List<Product> fetchedProducts = productRepository.findByCategoryId(Long.valueOf(categoryId));
+        if (fetchedProducts.isEmpty()) {
+            throw new EntityNotFoundException(Product.class, "categoryId", categoryId);
+        } else {
+            return fetchedProducts;
+        }
     }
-  }
+
+    public List<Product> findByKeyword(String name) {
+        List<Product> fetchedProducts = productRepository.findByNameContaining(name);
+        if (fetchedProducts.isEmpty()) {
+            throw new EntityNotFoundException(Product.class, "name", name);
+        } else {
+            return fetchedProducts;
+        }
+    }
 }
